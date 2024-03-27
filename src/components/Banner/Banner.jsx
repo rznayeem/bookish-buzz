6169ee65744fa9678d26807f145e8668d50ff90e
rznayeem <riyazulnayeem@gmail.com>
2024-03-28 02:22:17 +0600

    updated responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,9 +3,9 @@ import banner from '../../assets/banner.png';
 
 const Banner = () => {
   return (
-    <div className="lg:max-h-[554px] max-h-[450px] flex flex-col lg:flex-row justify-between items-center bg-[#1313130D] rounded-3xl lg:px-[120px] px-[20px] lg:py-[80px] py-[40px]">
-      <div>
-        <h1 className="playfair-font lg:text-[56px] text-[18px] font-bold">
+    <div className="lg:max-h-[554px] flex flex-col lg:flex-row justify-between items-center bg-[#1313130D] rounded-3xl lg:px-[120px] px-[20px] lg:py-[80px] py-[40px]">
+      <div className="flex flex-col items-center lg:items-start mb-5 lg:mb-0">
+        <h1 className="playfair-font lg:text-[56px] text-[40px] font-bold">
           Books to freshen up <br className="lg:flex hidden" /> your bookshelf
         </h1>
         <Link to={'/listed'}>
